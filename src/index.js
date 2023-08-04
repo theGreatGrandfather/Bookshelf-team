7111ef14476093e4fe291f2fdbc7fb4ff0b0db76
Vestanra <35749970+Vestanra@users.noreply.github.com>
2023-08-04 10:37:58 +0300

    markupForBooks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-// add your import from js file here
+// add your import from js file here 1
 
-// add your import from js file here
+// add your import from js file here 2
+
+// add your import from js file here 3
+
+import './js/markup-books';
+
 import './js/axios-get';
-// add your import from js file here
-
-// add your import from js file here
-
-// add your import from js file here
 
 // add your import from js file here
 
