06ee15c6bf35a22db9f024577e7a66c20f87a8ad
Elizaveta Taran <lizataran103@gmail.com>
2023-08-04 20:32:19 +0300

    default page

---
+++ src/js/6.js
@@ -0,0 +1,76 @@
+/**CARD TEMPLATE*/
+/**       <li class="shopping-list__item shopping-list-card">
+          <img
+            class="shopping-list-card__image"
+            src=""
+            alt=""
+            width=""
+            height=""
+          />
+          <div class="shopping-list-card__info">
+            <h2 class="shopping-list-card__title"></h2>
+            <p class="shopping-list-card__category"></p>
+            <p class="shopping-list-card__descr">
+            </p>
+            <div class="shopping-list-card__wrapper">
+              <p class="shopping-list-card__author"></p>
+              <div class="shopping-list-card__links">
+                <a
+                  href=""
+                  target="_blank"
+                  class="shopping-list-card__link --amazon _link"
+                ></a>
+                <a
+                  href=""
+                  target="_blank"
+                  class="shopping-list-card__link --apple _link"
+                ></a>
+                <a
+                  href=""
+                  target="_blank"
+                  class="shopping-list-card__link --barnes _link"
+                ></a>
+              </div>
+            </div>
+            <button class="shopping-list-card__button" type="button">
+              <svg
+                class="shopping-list-card__button--icon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+              >
+                <path
+                  d="M6 2H10M2 4H14M12.6667 4L12.1991 11.0129C12.129 12.065 12.0939 12.5911 11.8667 12.99C11.6666 13.3412 11.3648 13.6235 11.0011 13.7998C10.588 14 10.0607 14 9.00623 14H6.99377C5.93927 14 5.41202 14 4.99889 13.7998C4.63517 13.6235 4.33339 13.3412 4.13332 12.99C3.90607 12.5911 3.871 12.065 3.80086 11.0129L3.33333 4M6.66667 7V10.3333M9.33333 7V10.3333"
+                  stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
+        </li>
+ */
+
+//EMPTY-LIST PAGE TEMPLATE
+/**      <p class="shopping-list__text--empty">
+          This page is empty, add some books and proceed to order.
+        </p>
+        <picture>
+          <source
+            srcset="
+              ./images/shoplist-images/sh-list-265@1x.png 1x,
+              ./images/shoplist-images/sh-list-530@2x.png 2x
+            "
+            media="(max-width:767px)"
+          />
+          <source
+            srcset="
+              ./images/shoplist-images/sh-list-w-322@1x.png 1x,
+              ./images/shoplist-images/sh-list-w-644@2x.png 2x
+            "
+            media="(min-width:768px)"
+          />
+          <img src="./images/shoplist-images/sh-list-265@1x.png" alt="books" />
+        </picture> */
