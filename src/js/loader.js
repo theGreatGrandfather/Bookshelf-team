4229a72c7ffd6286656bea6319004dff859a0e3a
Vestanra <35749970+Vestanra@users.noreply.github.com>
2023-08-06 14:00:28 +0300

    loader

---
+++ src/js/loader.js
@@ -0,0 +1,25 @@
+const wrapLoader = document.querySelector('.wrapper-loader');
+const loader = document.querySelector('.loader');
+
+const loaderOn = () => {
+    loader.classList.remove('hidden-loader');
+    wrapLoader.classList.remove('hidden-loader');
+}
+
+const loaderOff = () => {
+    loader.classList.add('hidden-loader');
+    wrapLoader.classList.add('hidden-loader');
+}
+loaderOff()
+
+console.log(loader)
+
+export { loaderOn, loaderOff }
+
+
+
+// у функціях де іду запит на початку ставимо loaderOn,
+// у блок finally loaderOff
+
+//<div class="wrapper-loader"></div><div class="loader"></div>
+
