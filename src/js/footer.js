cc6d6c8744220b4074fa231f65d9671da656cead
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-08-07 16:44:59 +0300

    e

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,24 +1,24 @@
-const btn = document.querySelector('.btn-aaaaaa');
-const modal = document.getElementById('myModal');
-const closeModal = document.getElementById('closeModal');
+// const btn = document.querySelector('.btn-aaaaaa');
+// const modal = document.getElementById('myModal');
+// const closeModal = document.getElementById('closeModal');
 
-if (btn && modal && closeModal) {
-  const openModal = () => {
-    modal.classList.add('show');
-    document.body.style.overflow = 'hidden';
-  };
+// if (btn && modal && closeModal) {
+//   const openModal = () => {
+//     modal.classList.add('show');
+//     document.body.style.overflow = 'hidden';
+//   };
 
-  const closeModalFunction = () => {
-    modal.classList.remove('show');
-    document.body.style.overflow = 'auto';
-  };
+//   const closeModalFunction = () => {
+//     modal.classList.remove('show');
+//     document.body.style.overflow = 'auto';
+//   };
 
-  btn.addEventListener('click', openModal);
-  closeModal.addEventListener('click', closeModalFunction);
+//   btn.addEventListener('click', openModal);
+//   closeModal.addEventListener('click', closeModalFunction);
 
-  window.addEventListener('click', (event) => {
-    if (event.target === modal) {
-      closeModalFunction();
-    }
-  });
-}
+//   window.addEventListener('click', (event) => {
+//     if (event.target === modal) {
+//       closeModalFunction();
+//     }
+//   });
+// }
