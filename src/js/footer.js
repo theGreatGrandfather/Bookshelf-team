98434dce9b7019e863190cd6820aa8747d4dad88
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-08-08 00:11:18 +0300

    ewftyj
    jttutj

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,39 +1,39 @@
-const openModalButton = document.querySelector('.btn-aaaaaa');
-const closeModalButton = document.querySelector('.footer-btn-close');
-const modal = document.querySelector('.footer-modal');
+// const openModalButton = document.querySelector('.btn-aaaaaa');
+// const closeModalButton = document.querySelector('.footer-btn-close');
+// const modal = document.querySelector('.footer-modal');
 
-export function hiddenBodyOverflow() {
-  if (document.body.style.overflow === 'hidden') {
-    document.body.style.overflow = 'visible';
-  } else {
-    document.body.style.overflow = 'hidden';
-  }
-}
+// function hiddenBodyOverflow() {
+//   if (document.body.style.overflow === 'hidden') {
+//     document.body.style.overflow = 'visible';
+//   } else {
+//     document.body.style.overflow = 'hidden';
+//   }
+// }
 
-function closeModal() {
-  modal.classList.remove('show');
-  hiddenBodyOverflow();
-  document.removeEventListener('keydown', onEscKeyPress);
-}
+// function closeModal() {
+//   modal.classList.remove('show');
+//   hiddenBodyOverflow();
+//   document.removeEventListener('keydown', onEscKeyPress);
+// }
 
-function onEscKeyPress(event) {
-  if (event.key === 'Escape') {
-    closeModal();
-  }
-}
+// function onEscKeyPress(event) {
+//   if (event.key === 'Escape') {
+//     closeModal();
+//   }
+// }
 
-openModalButton.addEventListener('click', () => {
-  modal.classList.add('show');
-  hiddenBodyOverflow();
-  document.addEventListener('keydown', onEscKeyPress);
-});
+// openModalButton.addEventListener('click', () => {
+//   modal.classList.add('show');
+//   hiddenBodyOverflow();
+//   document.addEventListener('keydown', onEscKeyPress);
+// });
 
-closeModalButton.addEventListener('click', () => {
-  closeModal();
-});
+// closeModalButton.addEventListener('click', () => {
+//   closeModal();
+// });
 
-modal.addEventListener('click', (event) => {
-  if (event.target === modal || event.target.classList.contains('footer-overlay')) {
-    closeModal();
-  }
-});
+// modal.addEventListener('click', (event) => {
+//   if (event.target === modal || event.target.classList.contains('footer-overlay')) {
+//     closeModal();
+//   }
+// });
