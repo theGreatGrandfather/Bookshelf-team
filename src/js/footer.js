62d32d05b80ede5877bfd90adf30dc0ce9f573d7
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-08-07 16:43:55 +0300

    add

---
+++ src/js/footer.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector('.btn-aaaaaa');
+const modal = document.getElementById('myModal');
+const closeModal = document.getElementById('closeModal');
+
+if (btn && modal && closeModal) {
+  const openModal = () => {
+    modal.classList.add('show');
+    document.body.style.overflow = 'hidden';
+  };
+
+  const closeModalFunction = () => {
+    modal.classList.remove('show');
+    document.body.style.overflow = 'auto';
+  };
+
+  btn.addEventListener('click', openModal);
+  closeModal.addEventListener('click', closeModalFunction);
+
+  window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      closeModalFunction();
+    }
+  });
+}
