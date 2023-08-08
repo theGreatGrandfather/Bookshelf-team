d26ab5249a46ef19a11d1b450dd7f32eb4d258b9
Praded3 <pradid.store@gmail.com>
2023-08-09 00:59:17 +0300

    fix

--- src/js/orderModal.js
+++ src/js/orderModal.js
@@ -1,12 +1,42 @@
 const body = document.querySelector('body');
 const modal = document.querySelector('.modal__form__backdrop');
 const confirmBtn = document.querySelector('.form__button_submit');
+const closeBnt = document.querySelector('.modal__form__close__bt');
+const backdrop = document.querySelector('.modal__form__backdrop');
+
 
 const onSBtnClick = (e) => {
     e.preventDefault();
     console.log('e', e);
+    body.classList.toggle('no-scroll-js');
+    modal.classList.toggle('is-hiden');
+    document.addEventListener('keydown', onCloseModalESC);
+    closeBnt.addEventListener('click', onModalCloseBtn);
+    backdrop.addEventListener('click', onBackdropClick);
+}; 
 
-} 
+const onBackdropClick = (e) => {
+    if (e.target === e.currentTarget) {
+        doModalClose();
+    }
 
+}
+
+const onModalCloseBtn = (e) => {
+    doModalClose();
+}
+
+const onCloseModalESC = (e) => {
+    if (e.key === 'Escape') {
+        doModalClose();
+    }
+}
+const doModalClose = () => {
+    body.classList.toggle('no-scroll-js');
+    modal.classList.toggle('is-hiden');
+    document.removeEventListener('keydown', onCloseModalESC);
+    backdrop.removeEventListener('click', onBackdropClick);
+    closeBnt.removeEventListener('click', onModalCloseBtn);
+};
 
 confirmBtn.addEventListener('click', onSBtnClick);
