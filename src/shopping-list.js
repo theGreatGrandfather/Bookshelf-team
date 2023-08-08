0b9a8ebc221b4df814129f812c882b5c85ce01f3
Praded3 <pradid.store@gmail.com>
2023-08-09 01:37:31 +0300

    fix

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -9,11 +9,11 @@ import './js/toggleTheme.js';
 import './js/createFoundsMarcup';
 import './js/orderModal.js';
 import './js/auth-for-all-pages';
-
+import './js/google';
 
 // import './js/onFondsBtnClick.js';
 // import './js/authforshoping.js';
-import './js/orderModal.js'
+// import './js/orderModal.js'
 
 import './js/auth-for-all-pages';
 
