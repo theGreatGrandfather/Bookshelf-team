ef4b9228d133dc0d284301a639a450e3249cd88b
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-08-10 17:26:05 +0300

    fix

--- src/js/get-top-books.js
+++ src/js/get-top-books.js
@@ -5,7 +5,7 @@ import { loaderOn, loaderOff } from './loader';
 import { Notify } from "notiflix";
 
 export const mainBookList = document.querySelector('.best_list');
-let numBooksPerRow = 5;
+let numBooksPerRow = determineNumBooksPerRow();
 let savedMarkup = null;
 
 const debounce = (func, delay) => {
@@ -20,6 +20,28 @@ const shouldRenderMarkup = () => {
   return !categoryTitle.classList.contains('no-change');
 };
 
+const groupCategories = (data) => {
+  const grouped = [];
+  for (let i = 0; i < data.length; i += 3) {
+    grouped.push(data.slice(i, i + 3));
+  }
+  return grouped;
+};
+
+const renderMarkupGroup = (group) => {
+  const markup = group.map(({ books, list_name }) => {
+    const booksSlice = books.slice(0, numBooksPerRow);
+    return `<li class='best-item _list _anim-items'>
+              <h3 class='min-title'>${list_name}</h3>
+              <ul class='books-list'>
+                  ${makeMarkupForBooks(booksSlice)}
+              </ul>
+              <button class='see_more' data-name="${list_name}">See More</button>
+            </li>`;
+  });
+  mainBookList.insertAdjacentHTML('beforeend', markup.join(''));
+};
+
 const renderStartMarkup = async () => {
   if (!shouldRenderMarkup()) return;
 
@@ -28,20 +50,26 @@ const renderStartMarkup = async () => {
     const dataMarkup = await getTopBooks();
     if (dataMarkup.length === 0) return;
 
-    const firstIndexBooks = dataMarkup.map(({ books, list_name }) => {
-      const booksSlice = books.slice(0, numBooksPerRow);
-      return `<li class='best-item _list _anim-items'>
-                <h3 class='min-title'>${list_name}</h3>
-                <ul class='books-list'>
-                    ${makeMarkupForBooks(booksSlice)}
-                </ul>
-                <button class='see_more' data-name="${list_name}">See More</button>
-              </li>`;
-    });
+    const groupedData = groupCategories(dataMarkup);
+    const initialGroups = groupedData.slice(0, 2); // Починаючи з 2 груп
 
-    mainBookList.innerHTML = firstIndexBooks.join('');
+    for (const group of initialGroups) {
+      renderMarkupGroup(group);
+    }
+
+    const remainingGroups = groupedData.slice(2); // Решта груп
+    let currentIndex = 0;
+
+    const renderRemainingGroups = () => {
+      if (currentIndex >= remainingGroups.length) return;
+      renderMarkupGroup(remainingGroups[currentIndex]);
+      currentIndex++;
+      setTimeout(renderRemainingGroups, 2000); // Інтервал рендерингу наступної групи (2 секунди)
+    };
+
+    renderRemainingGroups();
   } catch (error) {
-    Notify.failure(`Error: ${errorMessage}`);
+    Notify.failure(`Error: ${error.message}`);
   } finally {
     loaderOff();
   }
@@ -73,6 +101,9 @@ const restoreMarkup = () => {
 
 window.addEventListener('resize', restoreMarkup);
 
+// Ця подія виконується після завантаження сторінки
+// Викликати функцію для рендерингу категорій
 window.addEventListener('load', () => {
+  renderStartMarkup();
   handleResize();
 });
